8c6328034aa5a5000fd04fa6ecb207a64842ae09
Michael Munyaneza <kuamchiz@gmail.com>
2021-11-01 19:38:44 -0700

    Display calculator results on the same page instead of the alert popup.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// This is the business logic:
-
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -10,13 +8,11 @@ function subtract(number1, number2) {
 
 function multiply(number1, number2) {
   return number1 * number2;
-}
+};
 
 function divide(number1, number2) {
   return number1 / number2;
-}
-
-// Everything below this line is the user interface logic:
+};
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
@@ -24,6 +20,6 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    alert(result);
   });
 });
