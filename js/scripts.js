84a2298109cde47466ac713d25ea51963b8fe264
TulaSabes <sabestula14@gmail.com>
2021-11-01 20:51:13 -0700

    Adding button function

--- js/scripts.js
+++ js/scripts.js
@@ -17,16 +17,32 @@ function divide(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
     const result = add(number1, number2);
     $("#output").text(result);
-  });
-  (document).ready(function() {
+ 
+  
     $("form#divide").submit(function(event) { 
       event.preventDefault();
-      const number1 = parseInt($("#add1").val());
-      const number2 = parseInt($("#add2").val());
-      const result = add(number1, number2);
+      const number1 = parseInt($("#number1").val());
+      const number2 = parseInt($("#number2").val());
+      const result = divide(number1, number2);
       $("#output").text(result);
+
+    $("form#subtract").submit(function(event) { 
+      event.preventDefault();
+      const number1 = parseInt($("#number1").val());
+      const number2 = parseInt($("#number2").val());
+      const result = subtract(number1, number2);
+      $("#output").text(result);
+ 
+    $("form#multiply").submit(function(event) { 
+      event.preventDefault();
+      const number1 = parseInt($("#number1").val());
+      const number2 = parseInt($("#number2").val());
+      const result = multiply(number1, number2);
+      $("#output").text(result);
+
+
 });
